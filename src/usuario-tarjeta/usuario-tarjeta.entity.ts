44b9fd2222dfc41971ea23b153e1067b5568a05d
magallegos1996 <bwcekos@gmail.com>
2018-07-24 19:15:49 -0500

    Entidad Usuario-Tarjeta

--- src/usuario-tarjeta/usuario-tarjeta.entity.ts
+++ src/usuario-tarjeta/usuario-tarjeta.entity.ts
@@ -1,7 +1,10 @@
-import { Entity } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('USUARIO_TARJETA')
 export class UsuarioTarjetaEntity {
+
+  @PrimaryGeneratedColumn()
+  id:number;
 
 
 
