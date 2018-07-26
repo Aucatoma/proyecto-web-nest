2abe8b728e132e1c93440c2f68d52ba4eb85f956
magallegos1996 <bwcekos@gmail.com>
2018-07-26 09:56:30 -0500

    Schema de credit card y servicio del libro

--- src/libro/libro.service.ts
+++ src/libro/libro.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { LibroEntity } from './libro.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+
 
 @Injectable()
-export class LibroService {}
+export class LibroService {
+  constructor(
+    @InjectRepository(LibroEntity)
+    private readonly _libroRepository: Repository<LibroEntity>,
+  ){}
+
+  async findAll(): Promise<LibroEntity[]>{
+    return await this._libroRepository.find();
+  }
+
+  async findByLibroId(id: string): Promise<LibroEntity>{
+    return await this._libroRepository.findOne(id);
+  }
+
+  async findByLibroName(nombre: string): Promise<LibroEntity[]>{
+    return await this._libroRepository.find({nombre:nombre});
+  }
+
+
+}
--- src/tarjeta-credito/tarjeta-credito.schema.ts
+++ src/tarjeta-credito/tarjeta-credito.schema.ts
@@ -10,18 +10,13 @@ export const TARJETA_SCHEMA = Joi.object()
     codigo: Joi
       .number()
       .integer()
-      .length(3)
       .required(),
     mes: Joi
       .number()
       .integer()
-      .length(2)
-      .greaterThan(0)
-      .lessThan(13)
       .required(),
     anio: Joi
       .number()
       .integer()
-      .length(2)
       .required(),
   });
